de3b24f92313bf9fecfe945d3450a03113b65dec
harmonit <harmonit email>
2025-04-05 12:11:41 +0800

    7

--- script.js
+++ script.js
@@ -95,58 +95,32 @@ function changeImage1(){
     },2000);
 }
 changeImage1();
-/*
-function changeImage2(){
-    if(index2>=images.length){
-        index2=0;
-    }
-    let img2=document.getElementsByClassName('img2')[0].getElementsByTagName('img')[0];
-    // console.log(img1,typeof img1);
-    img2.classList.add('fade-in');
-    setTimeout(() => {
-        img2.classList.remove('fade-in');
-        img2.classList.add('fade-out');
-        setTimeout(() => {
-            img2.classList.remove('fade-out');
-            img2.src=images[index2];
-            index2++;
-            setTimeout(changeImage2, 1000);
-        }, 1000);
-    },1000);
-}
-changeImage2();
-*/
 
+// 获取所有的 content 页面
+const contents = document.querySelectorAll('.content');
+// 当前页面索引
+let currentPageIndex = 0;
+window.addEventListener('wheel', function(event) {
+    let s = window.scrollY || document.documentElement.scrollTop;
 
-/*
-function changeImage(){
-    if(index2>=images.length){
-        index2=0;
-    }
-    let imgContainers = document.getElementsByClassName('img');
-    let imgList = [];
-    // 从每个 div 中获取 img 标签
-    //直接选择tagname也不行，因为foreach只支持用array类型的数列，直接选择返回的是HTMLCollection对象，还需要用for循环转换
-    for (let i = 0; i < 5 && i < imgContainers.length; i++) {
-        let img = imgContainers[i].getElementsByTagName('img')[0];   
-        if (img) {
-            imgList.push(img);
+    // 判断滚动方向并更新当前页面索引
+    if (event.deltaY > 0) {
+        // 向下滚动
+        if (currentPageIndex < contents.length - 1) {
+            currentPageIndex++;
+        }
+    } else {
+        // 向上滚动
+        if (currentPageIndex > 0) {
+            currentPageIndex--;
         }
     }
-    // imgList.forEach(i=>console.log(i));
-    imgList.forEach(img=>img.classList.add('fade-in'));//forEach循环，对列表内每个元素执行淡出动画
-    setTimeout(()=>{
-        imgList.forEach(img=>img.classList.remove('fade-in'));//淡出动画结束后，对列表内每个元素执行淡入动画
-        imgList.forEach(img=>img.classList.add('fade-out'));
-        setTimeout(()=>{
-            imgList.forEach(img=>img.classList.remove('fade-out'));//淡入动画结束后，对列表内每个元素执行淡出动画
-            imgList.forEach(img=>img.src=images[index2]);
-            index2++;
-            setTimeout(changeImage,1000);//动画间隔时间
-        },500);//淡出时间
-    },800);//淡入时间
-}
-changeImage();
-*/
+
+    // 滚动到目标页面
+    window.scrollTo({   
+        top: (contents[currentPageIndex].offsetTop),
+        behavior: 'smooth'
+    });
+}); 
 
 
